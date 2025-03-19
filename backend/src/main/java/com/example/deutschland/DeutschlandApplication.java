173677085ac50b3080806dbe48be7aa1795e2801
Piyush Dhanwal <piyushdhanwal2003@gmail.com>
2025-03-20 01:33:35 +0530

    first commit

---
+++ backend/src/main/java/com/example/deutschland/DeutschlandApplication.java
@@ -0,0 +1,13 @@
+package com.example.deutschland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeutschlandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeutschlandApplication.class, args);
+	}
+
+}

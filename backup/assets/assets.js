68efbb0feb8c8b29a02c2d4078a172b707799833
Piyush Dhanwal <piyushdhanwal2003@gmail.com>
2025-04-27 21:49:11 +0530

    google-calendar

---
+++ backup/assets/assets.js
@@ -0,0 +1,36 @@
+import logo from './logo.png'
+import First from './first.png'
+import Second from './second.png'
+import Third from './third.png'
+import Four from './four.png'
+import Five from './five.jpg'
+import Six from './six.jpg'
+import Seven from './seven.jpg'
+import Eight from './eight.jpg'
+import Nine from './nine.jpg'
+import Ten from './ten.jpg'
+import Eleven from './eleven.jpg'
+import dropdown_icon from './dropdown_icon.svg'
+import profile_pic from './profile_pic.png'
+
+
+export const assets ={
+    logo,
+    First,
+    Second,
+    Third,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Eleven,
+    dropdown_icon,
+    profile_pic
+
+
+}
+
+
